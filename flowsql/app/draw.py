68ed83890b1da1dd79da1b0d75a82a97cb15639f
domherriott <dherriott@deloitte.co.uk>
2023-02-07 14:09:48 +0000

    autodocs

--- flowsql/app/draw.py
+++ flowsql/app/draw.py
@@ -1,3 +1,4 @@
+"""draw.py."""
 import pickle
 from jinja2 import Environment, FileSystemLoader
 import os
@@ -6,6 +7,13 @@ from pathlib import Path
 
 
 def gen_drawing(tables):
+    """_summary_
+    Args:
+        tables (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     file_loader = FileSystemLoader("flowsql/app/templates")
     env = Environment(loader=file_loader)
 
@@ -16,11 +24,20 @@ def gen_drawing(tables):
 
 
 def save_drawing(output, output_filename):
+    """_summary_
+    Args:
+        output (_type_): _description_
+        output_filename (_type_): _description_
+    """
     with open(output_filename, "w+") as f:
         f.write(output)
 
 
 def open_drawing(output_filename):
+    """_summary_
+    Args:
+        output_filename (_type_): _description_
+    """
     try:
         os.system("open {}".format(output_filename))
     except:
@@ -28,11 +45,23 @@ def open_drawing(output_filename):
 
 
 def save_drawing(output, output_filename):
+    """_summary_
+
+    Args:
+        output (_type_): _description_
+        output_filename (_type_): _description_
+    """
     with open(output_filename, "w+") as f:
         f.write(output)
 
 
 def main(working_dir: Path, output_filename: Path):
+    """_summary_
+
+    Args:
+        working_dir (Path): _description_
+        output_filename (Path): _description_
+    """
 
     # reload object from file
     with open(working_dir / "tables.pkl", "rb") as f:
